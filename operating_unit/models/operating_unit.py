cbf29e76c8842fdd929b9334a47fb1674d1664fe
Andreas Stauder <andreas.stauder@braintec-group.com>
2019-08-16 16:11:43 +0200

    Migration of sv_planning to 13.0

--- operating_unit/models/operating_unit.py
+++ operating_unit/models/operating_unit.py
@@ -52,7 +52,6 @@ class OperatingUnit(models.Model):
         self.clear_caches()
         return res
 
-    @api.multi
     def write(self, vals):
         self.clear_caches()
         return super(OperatingUnit, self).write(vals)
