dc253e8a5ac2f05edf058d35457eaab0f525d6ba
BT-aestebanez <alvaro.estebanez@braintec-group.com>
2019-10-16 16:46:00 +0200

    [t115423] Improving mail creation

--- models/mail_mail_ext.py
+++ models/mail_mail_ext.py
@@ -24,18 +24,33 @@ class MailMailExt(models.Model):
                 record = self.env[model].browse(res_id)
                 if getattr(record, 'operating_unit_id', False):
                     operating_unit = record.operating_unit_id
-            if not operating_unit and self.env.context.get('uid', False):
-                user = self.env['res.users'].browse(self.env.context.get('uid'))
+            if not operating_unit:
+                if self.env.context.get('uid', False):
+                    user = self.env['res.users'].browse(
+                        self.env.context.get('uid')
+                    )
+                else:
+                    user = self.env.user
                 operating_unit = user.operating_unit_for_mails_id
 
-            if operating_unit and operating_unit.catchall_alias and operating_unit.catchall_domain:
-                sender_email_address = (operating_unit.catchall_alias + '@' +
-                                        operating_unit.catchall_domain)
+            if (operating_unit and operating_unit.catchall_alias and
+                    operating_unit.catchall_domain):
+                sender_email_address = (
+                    operating_unit.catchall_alias + '@' +
+                    operating_unit.catchall_domain
+                )
                 email_name = operating_unit.partner_id.name
-                mail.email_from = formataddr((email_name, sender_email_address))
-                mail.reply_to = mail.email_from
+                mail_from_reply_to = formataddr(
+                    (email_name, sender_email_address)
+                )
+                vals_to_write = {
+                    'email_from': mail_from_reply_to,
+                    'reply_to': mail_from_reply_to,
+                }
 
                 if operating_unit.outgoing_mail_server_id:
-                    mail.mail_server_id = operating_unit.outgoing_mail_server_id.id
+                    vals_to_write['mail_server_id'] = \
+                        operating_unit.outgoing_mail_server_id.id
+                mail.write(vals_to_write)
 
         return mails
