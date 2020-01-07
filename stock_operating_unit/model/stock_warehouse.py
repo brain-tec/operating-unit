7d526e5df77073d9266539a40c943fceeb148c28
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-07 08:25:55 +0100

    [MIG] stock_operating_unit: Migration to 13.0

--- stock_operating_unit/model/stock_warehouse.py
+++ stock_operating_unit/model/stock_warehouse.py
@@ -23,7 +23,6 @@ class StockWarehouse(models.Model):
         default=_default_operating_unit,
     )
 
-    @api.multi
     @api.constrains("operating_unit_id", "company_id")
     def _check_company_operating_unit(self):
         for rec in self:
@@ -43,7 +42,6 @@ class StockWarehouse(models.Model):
 class StockWarehouseOrderPoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    @api.multi
     @api.constrains("operating_unit_id", "warehouse_id", "location_id")
     def _check_location(self):
         for rec in self:
