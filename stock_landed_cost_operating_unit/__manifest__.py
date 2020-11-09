88c024669b93947449a0149d2f21d1c7be10486e
mara1 <raul.martin@braintec-group.com>
2020-11-09 10:57:13 +0100

    Automatically formatting the code according to pre-commit

--- stock_landed_cost_operating_unit/__manifest__.py
+++ stock_landed_cost_operating_unit/__manifest__.py
@@ -3,8 +3,7 @@
     "summary": "Adds the concept of operating unit (OU) in Landed Costs",
     "version": "13.0.1.0.0",
     "category": "Stock",
-    "author": "brain-tec AG,"
-              "Odoo Community Association (OCA)",
+    "author": "brain-tec AG," "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "depends": ["stock_landed_costs", "account_operating_unit"],
