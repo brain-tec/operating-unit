720beadd59c3c3e8d81c028c0deaefe0fe42c7a5
mara1 <raul.martin@braintec-group.com>
2021-03-22 15:16:39 +0100

    Merge branch '13.0.project_MI_465' into 13.0_v2
    # Conflicts:
    #	stock_available_immediately/README.rst
    #	stock_available_immediately/__manifest__.py
    #	stock_available_immediately/i18n/stock_available_immediately.pot
    #	stock_available_immediately/models/product_product.py
    #	stock_available_immediately/readme/CONTRIBUTORS.rst
    #	stock_available_immediately/static/description/index.html
    #	stock_available_immediately/tests/test_stock_available_immediately.py

--- sale_stock_operating_unit/models/stock_move.py
+++ sale_stock_operating_unit/models/stock_move.py
@@ -15,6 +15,11 @@ class StockMove(models.Model):
         values = super(StockMove, self)._get_new_picking_values()
         # Adding operating_unit_id as for 2 steps deliveries/receipts,
         # one of the pickings is not associated to the sale line
-        values.update({"operating_unit_id": self.sale_line_id.operating_unit_id.id or self.operating_unit_id.id})
+        values.update(
+            {
+                "operating_unit_id": self.sale_line_id.operating_unit_id.id
+                or self.operating_unit_id.id
+            }
+        )
 
         return values
