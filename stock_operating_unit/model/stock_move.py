d3f519c3721020d6ea94d74128f8524a84d2aafc
Jose Montero <jose.montero@brain-tec.ch>
2021-10-03 18:48:55 +0200

    [bt#16457] Stock move lines have now operating units stored

--- stock_operating_unit/model/stock_move.py
+++ stock_operating_unit/model/stock_move.py
@@ -1,5 +1,6 @@
 # Copyright 2019 ForgeFlow S.L.
 # Copyright 2019 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2020 brain-tec group
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
@@ -9,35 +10,47 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     operating_unit_id = fields.Many2one(
-        'operating.unit',
-        compute="_compute_operating_unit_id", store=True,
-        string='Source Location Operating Unit',
+        "operating.unit",
+        compute="_compute_operating_unit_id",
+        store=True,
+        string="Source Location Operating Unit",
     )
 
     operating_unit_dest_id = fields.Many2one(
-        'operating.unit',
+        "operating.unit",
         compute="_compute_operating_unit_dest_id",
-        string='Dest. Location Operating Unit',
+        store=True,
+        string="Dest. Location Operating Unit",
     )
 
-    @api.depends('location_id', 'location_id.operating_unit_id',
-                 'picking_type_id', 'picking_type_id.warehouse_id',
-                 'picking_type_id.warehouse_id.operating_unit_id')
+    @api.depends(
+        "location_id",
+        "location_id.operating_unit_id",
+        "picking_type_id",
+        "picking_type_id.warehouse_id",
+        "picking_type_id.warehouse_id.operating_unit_id",
+    )
     def _compute_operating_unit_id(self):
         for item in self:
             # For dropshipping
-            item.operating_unit_id = \
-                item.location_id.operating_unit_id or \
-                item.picking_type_id.warehouse_id.operating_unit_id
+            item.operating_unit_id = (
+                item.location_id.operating_unit_id
+                or item.picking_type_id.warehouse_id.operating_unit_id
+            )
 
-    @api.depends('location_id', 'location_id.operating_unit_id',
-                 'picking_type_id', 'picking_type_id.warehouse_id',
-                 'picking_type_id.warehouse_id.operating_unit_id')
+    @api.depends(
+        "location_id",
+        "location_id.operating_unit_id",
+        "picking_type_id",
+        "picking_type_id.warehouse_id",
+        "picking_type_id.warehouse_id.operating_unit_id",
+    )
     def _compute_operating_unit_dest_id(self):
         for item in self:
-            item.operating_unit_dest_id = \
-                item.location_dest_id.operating_unit_id or \
-                item.picking_type_id.warehouse_id.operating_unit_id
+            item.operating_unit_dest_id = (
+                item.location_dest_id.operating_unit_id
+                or item.picking_type_id.warehouse_id.operating_unit_id
+            )
 
     @api.constrains("picking_id", "location_id", "location_dest_id")
     def _check_stock_move_operating_unit(self):
@@ -48,8 +61,8 @@ class StockMove(models.Model):
             if ou_src and ou_pick and (ou_src != ou_pick) and (ou_dest != ou_pick):
                 raise UserError(
                     _(
-                        "Configuration error. The Stock moves must "
-                        "be related to a location (source or destination) "
-                        "that belongs to the requesting Operating Unit."
+                        "Configuration error. The Stock Moves must "
+                        "be related to locations (source and destination) "
+                        "that belong to the requesting Operating Unit."
                     )
                 )
