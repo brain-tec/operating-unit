58bf4743b84f22334eb1f854ce9a56a86fe7698e
BT-rmartin <raul.martin@bt-group.com>
2022-01-24 16:30:41 +0100

    [t126547] Travis bugfixes

--- purchase_operating_unit/models/purchase_order.py
+++ purchase_operating_unit/models/purchase_order.py
@@ -93,12 +93,17 @@ class PurchaseOrder(models.Model):
                     )
                 )
 
-    @api.onchange('picking_type_id')
+    @api.onchange("picking_type_id")
     def _onchange_picking_type_id(self):
         res = super()._onchange_picking_type_id()
         if self.picking_type_id:
-            if self.picking_type_id.warehouse_id.operating_unit_id.id != self.operating_unit_id.id:
-                self.operating_unit_id = self.picking_type_id.warehouse_id.operating_unit_id
+            if (
+                self.picking_type_id.warehouse_id.operating_unit_id.id
+                != self.operating_unit_id.id
+            ):
+                self.operating_unit_id = (
+                    self.picking_type_id.warehouse_id.operating_unit_id
+                )
         return res
 
     @api.onchange("operating_unit_id")
