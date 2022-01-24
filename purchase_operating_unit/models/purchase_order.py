ea9a96dbf29f072a535c50ea6a5dccdb91fbb28e
BT-rmartin <raul.martin@bt-group.com>
2022-01-24 16:13:00 +0100

    [t126547] Removing change on requesting_operating_unit_id, to continue working as in OCA standard Making code more robust

--- purchase_operating_unit/models/purchase_order.py
+++ purchase_operating_unit/models/purchase_order.py
@@ -95,9 +95,11 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('picking_type_id')
     def _onchange_picking_type_id(self):
-        super()._onchange_picking_type_id()
-        self.operating_unit_id = self.picking_type_id.warehouse_id.operating_unit_id
-        self.requesting_operating_unit_id = self.picking_type_id.warehouse_id.operating_unit_id
+        res = super()._onchange_picking_type_id()
+        if self.picking_type_id:
+            if self.picking_type_id.warehouse_id.operating_unit_id.id != self.operating_unit_id.id:
+                self.operating_unit_id = self.picking_type_id.warehouse_id.operating_unit_id
+        return res
 
     @api.onchange("operating_unit_id")
     def _onchange_operating_unit_id(self):
